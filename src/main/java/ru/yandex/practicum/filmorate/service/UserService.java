be05c5bbc871c9506c6c73d0e8f286d7a297aef2
Alex-Galvinauskas <agalvinauskas@yandex.ru>
2025-10-14 08:59:25 +0300

    feat: реализовать FilmService и UserService с бизнес-логикой
    - Добавить проверку уникальности фильмов по названию и году
    - Реализовать проверку уникальности пользователей по email и логину
    - Добавить нормализацию данных пользователя
    - Обеспечить обработку исключительных ситуаций

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.service;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User createUser(User user);
+    List<User> getAllUsers();
+    User getUserById(Long id);
+    User updateUser(User user);
+}
