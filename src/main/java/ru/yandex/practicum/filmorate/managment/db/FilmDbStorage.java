d39963993e0d83c1c92f302f91790402433dc886
Alex-Galvinauskas <agalvinauskas@yandex.ru>
2025-12-11 18:23:14 +0300

    checkstyle: убрать неиспользуемый импорт

--- src/main/java/ru/yandex/practicum/filmorate/managment/db/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/managment/db/FilmDbStorage.java
@@ -8,7 +8,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.managment.inMemory.FilmStorage;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
