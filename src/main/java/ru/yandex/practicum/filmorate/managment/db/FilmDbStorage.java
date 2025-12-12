25bf40ee8a6f5a674a4341951f633d335f5ac00c
Alex-Galvinauskas <agalvinauskas@yandex.ru>
2025-12-12 18:27:15 +0300

    Update FilmDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/managment/db/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/managment/db/FilmDbStorage.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.filmorate.managment.inMemory.FilmStorage;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
