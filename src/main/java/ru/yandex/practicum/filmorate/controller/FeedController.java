1d6a6ee43d90f24cb30b5120816f75cf8d222655
Alex-Galvinauskas <agalvinauskas@yandex.ru>
2025-12-13 10:00:20 +0300

    Update FeedController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/FeedController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FeedController.java
@@ -12,14 +12,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-@Slf4j
 public class FeedController {
 
     private final FeedService feedService;
 
     @GetMapping("/{id}/feed")
     public ResponseEntity<List<FeedEventDTO>> getUserFeed(@PathVariable Long id) {
-        log.debug("Запрос ленты событий для пользователя {}", id);
         List<FeedEventDTO> feed = feedService.getUserFeed(id);
         return ResponseEntity.ok(feed);
     }
