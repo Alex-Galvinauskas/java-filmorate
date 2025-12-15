1187aa72d69e9353dee79deb92f9027cce73fc50
Alex-Galvinauskas <agalvinauskas@yandex.ru>
2025-12-15 19:59:30 +0300

    style: добавить закрывающую скобку

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -73,7 +73,8 @@ public class UserController extends AbstractController<UserDTO, UserService> {
     public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
         service.deleteUser(userId);
         return ResponseEntity.noContent().build();
-      
+    }
+    
     @GetMapping("/{id}/recommendations")
     public ResponseEntity<List<FilmDTO>> getRecommendations(@PathVariable Long id) {
         List<FilmDTO> recommendations = recommendationService.getRecommendations(id);
